8d18ed17a7f36589844bb5ef2904740571980f11
t112360140 <t112360140@ntut.org.tw>
2026-06-20 22:23:53 +0800

    clean something?

--- software/obj/default/ff16/source/ff.d
+++
@@ -1,8 +0,0 @@
-obj/default/ff16/source/ff.o: ff16/source/ff.c ff16/source/ff.h \
- ff16/source/ffconf.h ff16/source/diskio.h
-
-ff16/source/ff.h:
-
-ff16/source/ffconf.h:
-
-ff16/source/diskio.h:
--- software/obj/default/ff16/source/ffunicode.d
+++
@@ -1,6 +0,0 @@
-obj/default/ff16/source/ffunicode.o: ff16/source/ffunicode.c \
- ff16/source/ff.h ff16/source/ffconf.h
-
-ff16/source/ff.h:
-
-ff16/source/ffconf.h:
