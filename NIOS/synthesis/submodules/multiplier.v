6a73cca0fd911aefc8eb0f39b2ad6792c2495904
t112360140 <t112360140@ntut.org.tw>
2026-06-24 01:39:41 +0800

    create gh-pages

--- NIOS/synthesis/submodules/multiplier.v
+++
@@ -1,7 +0,0 @@
-module multiplier (
-    input  signed [31:0] dataa,
-    input  signed [31:0] datab,
-    output signed [31:0] result
-);
-    assign result = dataa * datab;
-endmodule
--- tools/BGM/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-python mid.py
-
-pause
